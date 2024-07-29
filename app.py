80f94a68e4887ad0ed2dbfdc2c54ebb8ce71338b
chandanlal92 <chandanlal18@gmail.com>
2024-07-29 16:54:04 +0200

    Adds flash application

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load('churn_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    predict_request = np.array(data['data'])
+    prediction = model.predict(predict_request)
+    return jsonify({'prediction': prediction.tolist()})
+
+if __name__ == '__main__':
+    app.run(debug=True)
