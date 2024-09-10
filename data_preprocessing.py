5d204f7c5f7a540745da1920cc099e5d55f5a994
chandanlal92 <chandanlal18@gmail.com>
2024-09-10 15:34:25 +0200

    Fixed fill NA issues

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,8 +1,15 @@
+
+import data_preprocessing
+from data_processing import preprocess_data
+from data_processing import data
+
+
 # Feature Engineering (if any specific feature creation is needed)
 def feature_engineering(data):
     # Example: Create a new feature
     data['TenurePerMonth'] = data['tenure'] / data['TotalCharges']
     return data
 
+
+
 data = feature_engineering(data)
-X_train, X_test, y_train, y_test = preprocess_data(data)
